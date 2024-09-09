42c7c2ff9c3bf0ef397f9544471f35bffa493229
Sixtus ojochegbe Joseph <sixtusjoseph3@gmail.com>
2024-09-09 14:07:42 +0100

    let

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div>
+      footer
+    </div>
+  );
+}
+
+export default Footer;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <main>
+      navbar
+    </main>
+  );
+}
+
+export default Navbar;
